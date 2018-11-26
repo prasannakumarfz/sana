7952bd0899e15fa875b3c9579247b98db36ec088
prasannakumarfz <45353506+prasannakumarfz@users.noreply.github.com>
2018-11-26 17:22:40 +0530

    Add files via upload

---
+++ 1 to 3.js
@@ -0,0 +1,16 @@
+for(var i=0;i<10;i++){
+    if(i==3)
+    continue;
+    console.log(i);
+}
+
+
+
+var names=["prasanna","krishna","jaya","anvesh"];
+var length=names.length;
+for(intialvalue=0;intialvalue<length;intialvalue++)
+{
+    if(intialvalue==2)
+    continue;
+    console.log(names[intialvalue]); 
+}
